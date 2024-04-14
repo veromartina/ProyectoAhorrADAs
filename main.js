7ea13fa335852f4b73e3c1763af9f839c05c64fc
veromartina <veromartina8@gmail.com>
2024-04-14 19:23:02 -0300

    se corrije main.js para lograr el despliegue del menu hamburguesa. se modifican algunos estilos

--- main.js
+++ main.js
@@ -10,11 +10,14 @@ let navItems = document.getElementById
 
 const ocultarOperaciones = document.getElementById("ocultar_operaciones")
 const nuevaOperacion = document.getElementById("nueva_operacion")
+const sectionBalance = document.getElementById("section-balance")
 
 iconoAbrir.addEventListener("click", () => { 
   iconoCerrar.style.display = "block";
   navItems.style.display ="block";
   iconoAbrir.style.display = "none";
+  sectionBalance.style.transform = "translateY(100px)"
+
 
 })
 
@@ -22,6 +25,7 @@ iconoCerrar.addEventListener("click", () => {
   iconoAbrir.style.display = "block";
   navItems.style.display ="none";
   iconoCerrar.style.display = "none";
+  sectionBalance.style.transform = "translateY(0px)"
 })
 
 // revisar
